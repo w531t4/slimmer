751f493a4314a827e734d0feadec6983c3c87aa3
Aaron White <w531t4@gmail.com>
2021-10-11 18:55:55 +0100

    Manage tclap dependency with cmake

---
+++ cmake/tclap.cmake
@@ -0,0 +1,55 @@
+set(target tclap_external)
+set(tclap_empty_tests_file "all:
+@:
+install:
+@:
+")
+file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/extras)
+file(WRITE ${CMAKE_BINARY_DIR}/extras/empty_makefile "${tclap_empty_tests_file}")
+ExternalProject_add(
+        ${target}
+        CMAKE_ARGS ""
+        LOG_CONFIGURE 1
+        LOG_BUILD 1
+        GIT_REPOSITORY https://github.com/mirror/tclap.git
+        GIT_TAG v1.2.4
+        GIT_PROGRESS 1
+        BUILD_IN_SOURCE 0
+        UPDATE_COMMAND ""
+        PATCH_COMMAND     "libtoolize"
+                          "--force"
+        COMMAND           "autoreconf"
+                          "-i"
+        CONFIGURE_COMMAND "<SOURCE_DIR>/configure"
+                          "--prefix=<INSTALL_DIR>"
+        COMMAND         "rm"
+                        "-rf"
+                        <BINARY_DIR>/tests
+                        <BINARY_DIR>/examples
+        COMMAND         "mkdir"
+                        "-p"
+                        <BINARY_DIR>/tests
+                        <BINARY_DIR>/examples
+                        <BINARY_DIR>/docs/html
+        COMMAND         "cp"
+                        "${CMAKE_BINARY_DIR}/extras/empty_makefile"
+                        <BINARY_DIR>/tests/Makefile
+        COMMAND         "cp"
+                        "${CMAKE_BINARY_DIR}/extras/empty_makefile"
+                        <BINARY_DIR>/examples/Makefile
+        BUILD_COMMAND   "make"
+                        "-j4"
+        INSTALL_COMMAND "mkdir"
+                        "-p"
+                        <INSTALL_DIR>/docs/html
+        COMMAND         "make"
+                        "install"
+)
+ExternalProject_Get_property(${target} INSTALL_DIR)
+
+# INTERFACE used here because this lib is header-only
+add_library(tclap INTERFACE)
+
+file(MAKE_DIRECTORY ${INSTALL_DIR}/include)
+target_include_directories(tclap INTERFACE ${INSTALL_DIR}/include)
+add_dependencies(tclap ${target})
